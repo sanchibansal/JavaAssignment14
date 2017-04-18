e172adc76803950482877824a56ef7873b64e212
sanchibansal <bansalsanchi77.sb@gmail.com>
2017-04-18 10:11:39 +0530

    assignment 4

---
+++ assignment 4/Ascii.java
@@ -0,0 +1,12 @@
+class Ascii
+{
+	public static void main(String args[])
+{
+	char c=65;		// initializing character value of c with 65	
+	int a=65;		//initializing int value of a with 65
+	for(a=65,c=65;a<91 && c<91;a++,c++)
+{	System.out.println(a+" - "+c);}			//  for loop to print all the ascci codes between 65 and 91 in tabular form
+
+
+}
+}
